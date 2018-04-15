3a9db359c39b826334faf537e9ea22164b858270
ruchimutneja <mutnejaruchi@gmail.com>
2018-04-15 23:35:05 +0530

    Add rspec test framework

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should have_and_belong_to_many(:categories)
+  should have_and_belong_to_many(:tags)
+  should have_and_belong_to_many(:atributes)
+  should have_many(:productImages)
 end
